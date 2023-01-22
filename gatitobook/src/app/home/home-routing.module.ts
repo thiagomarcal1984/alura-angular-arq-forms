15c066fe71d943b6a161fc2c7b2ac716dad52a5d
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-01-22 16:13:26 -0300

    Login.

--- gatitobook/src/app/home/home-routing.module.ts
+++ gatitobook/src/app/home/home-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,6 +6,12 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{
   path: '',
   component: HomeComponent,
+  children:[
+    {
+      path: '',
+      component: LoginComponent,
+    }
+  ]
 }];
 
 @NgModule({
