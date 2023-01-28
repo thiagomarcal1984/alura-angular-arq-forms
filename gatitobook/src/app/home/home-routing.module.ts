aeb635afba4c1b48d20b8d94786f1e8ab128f878
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-01-28 08:49:33 -0300

    Criando Componente.

--- gatitobook/src/app/home/home-routing.module.ts
+++ gatitobook/src/app/home/home-routing.module.ts
@@ -2,6 +2,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 const routes: Routes = [{
   path: '',
@@ -10,7 +11,11 @@ const routes: Routes = [{
     {
       path: '',
       component: LoginComponent,
-    }
+    },
+    {
+      path: 'novo-usuario',
+      component: NovoUsuarioComponent,
+    },
   ]
 }];
 
