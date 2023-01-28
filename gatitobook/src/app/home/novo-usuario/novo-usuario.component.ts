81495706ed86a927e66b154b1fe0214ec97e4735
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-01-28 13:20:09 -0300

    Validação de Formulário.

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 import { minusculoValidator } from './minusculo.validator';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -20,23 +21,28 @@ export class NovoUsuarioComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-   this.novoUsuarioForm = this.formBuilder.group({
-    email: ['', [
-      Validators.required,
-      Validators.email,
-    ]],
-    fullName: ['', [
-      Validators.required,
-      Validators.minLength(4),
-    ]],
-    userName: [
-      '',
-      [minusculoValidator],
-      [this.usuarioExisteService.usuarioJaExiste()],
-    ],
-    password: [''],
-   })
-  }
+   this.novoUsuarioForm = this.formBuilder.group(
+    {
+      email: ['', [
+        Validators.required,
+        Validators.email,
+      ]],
+      fullName: ['', [
+        Validators.required,
+        Validators.minLength(4),
+      ]],
+      userName: [
+        '',
+        [minusculoValidator],
+        [this.usuarioExisteService.usuarioJaExiste()],
+      ],
+      password: [''],
+    },
+    {
+      validators: [ usuarioSenhaIguaisValidator ],
+    }
+  );
+}
 
   cadastrar() {
     const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
