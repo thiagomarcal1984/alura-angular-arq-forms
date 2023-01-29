11b6e1b7f1d9d9e6c9c8e790ee1f98f731a8302b
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-01-29 11:37:56 -0300

    Serviço de Usuário.

---
+++ gatitobook/src/app/autenticacao/usuario/usuario.service.ts
@@ -0,0 +1,45 @@
+import { TokenService } from './../token.service';
+import { Injectable } from '@angular/core';
+import { Usuario } from './usuario';
+import jwt_decode from 'jwt-decode';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  private usuarioSubject = new BehaviorSubject<Usuario>({
+
+  })
+
+  constructor(private tokenService: TokenService) {
+    if (this.tokenService.possuiToken()) {
+      this.decodificaJWT();
+    }
+  }
+
+  private decodificaJWT() {
+    const token  = this.tokenService.retornaToken();
+    const usuario = jwt_decode(token) as Usuario;
+    // Os inscritos neste observable receberão um usuario:
+    this.usuarioSubject.next(usuario);
+  }
+
+  retornaUsuario() {
+    return this.usuarioSubject.asObservable();
+  }
+
+  salvaToken(token: string) {
+    this.tokenService.salvaToken(token);
+    this.decodificaJWT();
+  }
+
+  logout() {
+    this.tokenService.excluiToken();
+    this.usuarioSubject.next({}); // Dispara usuário vazio.
+  }
+
+  estaLogado() {
+    return this.tokenService.possuiToken();
+  }
+}
